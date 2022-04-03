cd4304955b1137e56a05ad0dfeb2c822d7333b4b
luiz gomes <luizgomes77777@gmail.com>
2022-04-03 02:09:52 -0300

    commit por medo da chuva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransactionsDashboardComponent } from './pages/transactions-dashboard/transactions-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Transactions',
+    component: TransactionsDashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/Transactions',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
+import { TransactionModalComponent } from './components/transaction-modal/transaction-modal.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TransactionsDashboardComponent } from './pages/transactions-dashboard/transactions-dashboard.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { DescriptionTransactionComponent } from './components/description-transaction/description-transaction.component';
+import { TransactionItemComponent } from './components/transaction-item/transaction-item.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionsTableComponent,
+    TransactionModalComponent,
+    HeaderComponent,
+    FooterComponent,
+    TransactionsDashboardComponent,
+    FilterPipe,
+    DescriptionTransactionComponent,
+    TransactionItemComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
